c9b29d8b7c84a011dd902038e6996578434e78c6
Zhongchao Qian <zq3684@rit.edu>
2019-10-07 17:06:36 -0400

    Add (ordered/sequential) ImageNet subsets

--- dataset.py
+++ dataset.py
@@ -1,7 +1,8 @@
 import os
 
-from torchvision.datasets import ImageNet
 import torchvision.transforms as transforms
+from torch.utils.data import Subset
+from torchvision.datasets import ImageNet
 
 
 def imagenet_1k():
@@ -25,3 +26,23 @@ def imagenet_1k():
             normalize,
         ]))
     return dataset_train, dataset_val
+
+
+def imagenet_subset(num_categories: int, *, random: bool):
+    if random:
+        raise NotImplementedError
+    ds_trn, ds_val = imagenet_1k()
+    accept_categories = list(range(num_categories))
+    subset_idx = [i for i in range(len(ds_trn)) if ds_trn.targets[i] in accept_categories]
+    ds_trn = Subset(ds_trn, subset_idx)
+    subset_idx = [i for i in range(len(ds_val)) if ds_val.targets[i] in accept_categories]
+    ds_val = Subset(ds_val, subset_idx)
+    return ds_trn, ds_val
+
+
+def imagenet_100():
+    return imagenet_subset(100, random=False)
+
+
+def imagenet_10():
+    return imagenet_subset(10, random=False)
